a47905675eb3198b80e4bfbba124a97bdfbbdd47
kolabrate <Ana1982(>
2017-03-05 20:51:40 +1100

    BASELEINE V0.0- With default controller. Baseline Commit. Please revert to this incase of issues. - Anand

---
+++ Topdf.api/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Topdf.api.Controllers
+{
+    public class DefaultController : ApiController
+    {
+
+        public List<string> NameList = new List<string>() {"Anand","Maran","Priya","Arjun"};
+
+        [HttpGet]
+        public IEnumerable<string> GetName()
+        {
+
+            return NameList.AsEnumerable();
+
+        }
+
+    }
+}
